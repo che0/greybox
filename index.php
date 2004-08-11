26e9c05ba5025f90800148379a0c94a8dd41143f
che0 <che0>
2004-08-11 01:43:12 +0000

    almost finished framework

--- index.php
+++ index.php
@@ -5,24 +5,21 @@ include_once("lib/config.inc");
 include_once("lib/cpykhen.inc");
 include_once("lib/dblink.inc");
 include_once("lib/page.inc");
-
-$_GLOBALS["page"] = new pg_page();
+$GLOBALS["page"] = new pg_page();
 include_once("lib/session.inc");
-
 
 include_once('inc/functions.php');
 
-/*
-<p class="link"><a href="./">{link_homepage}</a></p>
-<p class="link"><a href="clovek.php?akce=vsichni">{link_everyone}</a></p>
-<p class="link"><a href="klub.php">{link_clubs}</a></p>
-*/
+$GLOBALS["page"]->links[] = array("./",$lang["link_homepage"]);
+$GLOBALS["page"]->links[] = array("./",$lang["link_everyone"]);
+$GLOBALS["page"]->links[] = array("./",$lang["clubs"]);
 
+$GLOBALS["page"]->default_page = "default";
 
-$fw_stranky = array(
+$GLOBALS["page"]->pages = array(
 	//array(modul, titulek)
 );
 
-
+$GLOBALS["page"]->render();
 
 ?>
