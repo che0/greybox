3042cdbe939efc8506e6c540ba750aae5d23ebb5
che0 <che0>
2004-08-25 12:35:23 +0000

    minor

--- languages/czech.php
+++ languages/czech.php
@@ -1,7 +1,9 @@
 <?php
 // czech language definition file
 
-$lang['release notes'] = 'Tohle je první verze greyboxu, která by už mohla doopravdy něco dělat. Užijte si ji. Na chyby si stěžujte v <a href="http://sourceforge.net/tracker/?group_id=108503&amp;atid=650817">Bugzille</a>. P.N.';
+$lang['release notes'] = 'Tohle je první verze greyboxu, která by už mohla doopravdy něco dělat. Užijte si ji.<br />
+Na chyby si stěžujte v <a href="http://sourceforge.net/tracker/?group_id=108503&amp;atid=650817">Bugzille</a>.<br />
+O login si pište padre.amaro@inkvizice.org<br /> -- Petr Novák.';
 $lang['tail_footnote'] = 'greybox, <a href="http://greybox.sourceforge.net/">greybox.sourceforge.net</a> Copyright &copy; Petr Novák 2004<br />Distribuováno pod licencí <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>. Používá <a href="http://www.php.net/">PHP</a>, <a href="http://www.mysql.com/products/mysql/">MySQL</a> a <a href="http://www.christian-seiler.de/projekte/php/bbcode/">BB-Code parser Christiana Seilera</a>.<br />
 Stránky obsahují <a href="http://validator.w3.org/check?uri=referer">XHTML</a> a <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>, doporučen je tedy <a href="http://www.mozilla.org/products/firefox/">webový prohlížeč</a>.';
 
