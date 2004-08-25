ab71b5a8763e2a52311ea6a3a7530610c19ed2b6
che0 <che0>
2004-08-25 14:39:54 +0000

    eye candy, page sections

--- index.php
+++ index.php
@@ -8,63 +8,65 @@ include_once("lib/page.inc");
 include_once("lib/session.inc");
 include_once("lib/misc.inc");
 
-$GLOBALS["page"]->links[] = array("./",$lang["homepage"]);
-$GLOBALS["page"]->links[] = array("./?page=lide",$lang["people"]);
-$GLOBALS["page"]->links[] = array("./?page=kluby",$lang["clubs"]);
-$GLOBALS["page"]->links[] = array("./?page=tymy",$lang["teams"]);
-$GLOBALS["page"]->links[] = array("./?page=souteze",$lang["competitions"]);
-$GLOBALS["page"]->links[] = array("./?page=turnaje",$lang["tournaments"]);
-$GLOBALS["page"]->links[] = array("./?page=debaty",$lang["debates"]);
-$GLOBALS["page"]->links[] = array("./?page=teze",$lang["resolutions"]);
+// link := array(link, title, section)
+$GLOBALS["page"]->links[] = array("./",$lang["homepage"],"home");
+$GLOBALS["page"]->links[] = array("./?page=lide",$lang["people"],"clovek");
+$GLOBALS["page"]->links[] = array("./?page=kluby",$lang["clubs"],"klub");
+$GLOBALS["page"]->links[] = array("./?page=tymy",$lang["teams"],"tym");
+$GLOBALS["page"]->links[] = array("./?page=souteze",$lang["competitions"],"soutez");
+$GLOBALS["page"]->links[] = array("./?page=turnaje",$lang["tournaments"],"turnaj");
+$GLOBALS["page"]->links[] = array("./?page=debaty",$lang["debates"],"debata");
+$GLOBALS["page"]->links[] = array("./?page=teze",$lang["resolutions"],"teze");
 
-$GLOBALS["page"]->default_page = "default";
+// page := array(keyword, title, section)
+$GLOBALS["page"]->default_page = array("default",null, "home");
 
 $GLOBALS["page"]->pages = array(
-	array("language", $lang["language"]),
-	array("season", $lang["season"]),
-	array("login", $lang["login"]),
-	array("logout", $lang["login"]),
-	array("lide", $lang["people"]),
-	array("clovek", $lang["people"]),
-	array("clovek.add", $lang["add person"]),
-	array("clovek.edit.exec", $lang["edit person"]),
-	array("clovek.pw.edit.exec", $lang["edit person"]),
-	array("clovek.accr.edit.exec", $lang["edit person"]),
-	array("clovek.cont.edit.exec", $lang["edit person"]),
-	array("clovek.self.exec", $lang["edit person"]),
-	array("klub", $lang["clubs"]),
-	array("klub.add", $lang["add club"]),
-	array("klub.clovek.edit.exec", $lang["edit club"]),
-	array("klub.edit.exec", $lang["edit club"]),
-	array("kluby", $lang["clubs"]),
-	array("teze", $lang["resolutions"]),
-	array("teze.add", $lang["add resolution"]),
-	array("teze.detaily", $lang["resolution details"]),
-	array("teze.edit.exec", $lang["edit resolution"]),
-	array("teze.add.exec", $lang["add resolution"]),
-	array("souteze", $lang["competitions"]),
-	array("soutez", $lang["competition"]),
-	array("soutez.kidy", $lang["competition"]."/".$lang["kidy"]),
-	array("soutez.edit.exec", $lang["edit competition"]),
-	array("soutez.add", $lang["add competition"]),
-	array("soutez.teze.edit.exec", $lang["edit official resolution"]),
-	array("tym.soutez", $lang["team in the competition"]),
-	array("turnaj", $lang["tournament"]),
-	array("turnaj.add", $lang["add tournament"]),
-	array("turnaj.edit.exec", $lang["edit tournament"]),
-	array("turnaje", $lang["tournaments"]),
-	array("turnaj.clovek.edit.exec", $lang["edit tournament"]),
-	array("tymy", $lang["teams"]),
-	array("tym", $lang["team"]),
-	array("tym.add", $lang["add team"]),
-	array("tym.edit.exec", $lang["edit team"]),
-	array("tym.clovek.edit.exec", $lang["edit team"]),
-	array("debata", $lang["debate"]),
-	array("debata.add", $lang["add debate"]),
-	array("debata.edit", $lang["edit debate"]),
-	array("debata.clovek.edit.exec", $lang["edit debate"]),
-	array("debata.edit.exec", $lang["edit debate"]),
-	array("debaty", $lang["debates"]),
+	array("language", $lang["language"],"home"),
+	array("season", $lang["season"],"home"),
+	array("login", $lang["login"],"home"),
+	array("logout", $lang["login"],"home"),
+	array("lide", $lang["people"],"clovek"),
+	array("clovek", $lang["people"],"clovek"),
+	array("clovek.add", $lang["add person"],"clovek"),
+	array("clovek.edit.exec", $lang["edit person"],"clovek"),
+	array("clovek.pw.edit.exec", $lang["edit person"],"clovek"),
+	array("clovek.accr.edit.exec", $lang["edit person"],"clovek"),
+	array("clovek.cont.edit.exec", $lang["edit person"],"clovek"),
+	array("clovek.self.exec", $lang["edit person"],"clovek"),
+	array("klub", $lang["clubs"],"klub"),
+	array("klub.add", $lang["add club"], "klub"),
+	array("klub.clovek.edit.exec", $lang["edit club"], "klub"),
+	array("klub.edit.exec", $lang["edit club"], "klub"),
+	array("kluby", $lang["clubs"], "klub"),
+	array("teze", $lang["resolutions"], "teze"),
+	array("teze.add", $lang["add resolution"], "teze"),
+	array("teze.detaily", $lang["resolution details"], "teze"),
+	array("teze.edit.exec", $lang["edit resolution"], "teze"),
+	array("teze.add.exec", $lang["add resolution"], "teze"),
+	array("souteze", $lang["competitions"], "soutez"),
+	array("soutez", $lang["competition"], "soutez"),
+	array("soutez.kidy", $lang["competition"]."/".$lang["kidy"], "soutez"),
+	array("soutez.edit.exec", $lang["edit competition"], "soutez"),
+	array("soutez.add", $lang["add competition"], "soutez"),
+	array("soutez.teze.edit.exec", $lang["edit official resolution"], "soutez"),
+	array("tym.soutez", $lang["team in the competition"], "soutez"),
+	array("turnaj", $lang["tournament"], "turnaj"),
+	array("turnaj.add", $lang["add tournament"], "turnaj"),
+	array("turnaj.edit.exec", $lang["edit tournament"], "turnaj"),
+	array("turnaje", $lang["tournaments"], "turnaj"),
+	array("turnaj.clovek.edit.exec", $lang["edit tournament"], "turnaj"),
+	array("tymy", $lang["teams"], "tym"),
+	array("tym", $lang["team"], "tym"),
+	array("tym.add", $lang["add team"], "tym"),
+	array("tym.edit.exec", $lang["edit team"], "tym"),
+	array("tym.clovek.edit.exec", $lang["edit team"], "tym"),
+	array("debata", $lang["debate"], "debata"),
+	array("debata.add", $lang["add debate"], "debata"),
+	array("debata.edit", $lang["edit debate"], "debata"),
+	array("debata.clovek.edit.exec", $lang["edit debate"], "debata"),
+	array("debata.edit.exec", $lang["edit debate"], "debata"),
+	array("debaty", $lang["debates"], "debata"),
 );
 
 $GLOBALS["page"]->render();
