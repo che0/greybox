c9e9a9a9ce603f13a7c8e7e80037d3da2993a86d
che0 <che0>
2004-09-13 10:54:05 +0000

    debate team/speaker duplicity checks

--- languages/english.php
+++ languages/english.php
@@ -292,4 +292,7 @@ $lang['selected competition is locked'] = 'selected competition is locked';
 $lang['administrators'] = 'administrators';
 $lang['administrator'] = 'administrator';
 $lang['competition is locked'] = 'competition is locked';
+$lang['team cannot debate against itself'] = 'team cannot debate against itself';
+$lang['speaker %s is not a member of %s team, dropping'] = 'speaker %s is not a member of %s team, dropping';
+$lang['one person cannot debate twice in one debate (%s==%s), dropping %s'] = 'one person cannot debate twice in one debate (%s==%s), dropping %s';
 ?>
