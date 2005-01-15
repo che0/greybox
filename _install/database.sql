a07f7f13cb0211f06bf7391fc3453ce3048df21c
che0 <che0>
2005-01-15 15:14:12 +0000

    zmeny PK ve vztahovych tabulkach

--- _install/database.sql
+++ _install/database.sql
@@ -116,14 +116,13 @@ CREATE TABLE debata (
 --
 
 CREATE TABLE clovek_debata (
-  dc_ID int(10) unsigned NOT NULL auto_increment,
   debata_ID int(10) unsigned NOT NULL default '0',
   clovek_ID int(10) unsigned NOT NULL default '0',
   role enum('r','o','a1','a2','a3','n1','n2','n3') NOT NULL default 'r',
   kidy tinyint(3) unsigned default NULL,
   rozhodnuti tinyint(1) default NULL,
   presvedcive tinyint(1) default NULL,
-  PRIMARY KEY  (dc_ID),
+  PRIMARY KEY  (debata_ID, clovek_ID, role),
   KEY debata_ID (debata_ID),
   KEY clovek_ID (clovek_ID),
   constraint fk_cd_debata foreign key (debata_ID) references debata (debata_ID) on delete cascade,
@@ -135,13 +134,12 @@ CREATE TABLE clovek_debata (
 --
 
 CREATE TABLE clovek_debata_ibody (
-  cdi_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
   debata_ID int(10) unsigned NOT NULL default '0',
   rocnik tinyint(3) unsigned NOT NULL default '0',
   role enum('debater','rozhodci','trener','organizator') NOT NULL default 'debater',
   ibody decimal(5,3) NOT NULL default '0.000',
-  PRIMARY KEY  (cdi_ID),
+  PRIMARY KEY  (clovek_ID, debata_ID, role),
   KEY clovek_ID (clovek_ID),
   KEY debata_ID (debata_ID),
   KEY role (role),
@@ -154,12 +152,11 @@ CREATE TABLE clovek_debata_ibody (
 --
 
 CREATE TABLE clovek_klub (
-  ck_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
   klub_ID int(10) unsigned NOT NULL default '0',
   rocnik tinyint(3) unsigned NOT NULL default '0',
   role enum('t') NOT NULL default 't',
-  PRIMARY KEY  (ck_ID),
+  PRIMARY KEY  (clovek_ID, klub_ID, rocnik),
   KEY clovek_ID (clovek_ID),
   KEY klub_ID (klub_ID),
   constraint fk_ck_clovek foreign key (clovek_ID) references clovek (clovek_ID) on delete restrict,
@@ -171,12 +168,11 @@ CREATE TABLE clovek_klub (
 --
 
 CREATE TABLE clovek_turnaj (
-  ct_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
   turnaj_ID int(10) unsigned NOT NULL default '0',
   role enum('o') NOT NULL default 'o',
   mocnost tinyint(3) unsigned NOT NULL default '1',
-  PRIMARY KEY  (ct_ID),
+  PRIMARY KEY  (clovek_ID, turnaj_ID),
   KEY clovek_ID (clovek_ID),
   KEY turnaj_ID (turnaj_ID),
   constraint fk_ctu_clovek foreign key (clovek_ID) references clovek (clovek_ID) on delete restrict,
@@ -188,11 +184,10 @@ CREATE TABLE clovek_turnaj (
 --
 
 CREATE TABLE clovek_tym (
-  ct_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
   tym_ID int(10) unsigned NOT NULL default '0',
   aktivni tinyint(1) NOT NULL default '1',
-  PRIMARY KEY  (ct_ID),
+  PRIMARY KEY  (clovek_ID, tym_ID),
   KEY clovek_ID (clovek_ID),
   KEY tym_ID (tym_ID),
   constraint fk_cty_clovek foreign key (clovek_ID) references clovek (clovek_ID) on delete cascade,
@@ -205,12 +200,11 @@ CREATE TABLE clovek_tym (
 --
 
 CREATE TABLE debata_tym (
-  dt_ID int(10) unsigned NOT NULL auto_increment,
   debata_ID int(10) unsigned NOT NULL default '0',
   tym_ID int(10) unsigned NOT NULL default '0',
   pozice tinyint(1) NOT NULL default '0',
   body tinyint(3) unsigned default NULL,
-  PRIMARY KEY  (dt_ID),
+  PRIMARY KEY  (debata_ID, tym_ID),
   KEY debata_ID (debata_ID),
   KEY tym_ID (tym_ID),
   constraint fk_dt_debata foreign key (debata_ID) references debata (debata_ID) on delete cascade,
@@ -237,13 +231,11 @@ CREATE TABLE kontakt (
 --
 
 CREATE TABLE login (
-  login_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
   username varchar(32) NOT NULL default '',
   password varchar(35) NOT NULL default '',
-  PRIMARY KEY  (login_ID),
-  KEY clovek_ID (clovek_ID),
-  KEY username (username),
+  PRIMARY KEY  (clovek_ID),
+  UNIQUE username (username),
   constraint fk_login_clovek foreign key (clovek_ID) references clovek (clovek_ID) on delete cascade
 ) TYPE=InnoDB CHARSET=utf8 COLLATE=utf8_czech_ci;
 
@@ -252,12 +244,11 @@ CREATE TABLE login (
 --
 
 CREATE TABLE rozhodci (
-  rozhodci_ID int(10) unsigned NOT NULL auto_increment,
   clovek_ID int(10) unsigned NOT NULL default '0',
+  rocnik tinyint(3) unsigned NOT NULL default '0',
   misto varchar(255) default NULL,
   jazyk set('cz','en','de','fr') default NULL,
-  rocnik tinyint(3) unsigned NOT NULL default '0',
-  PRIMARY KEY  (rozhodci_ID),
+  PRIMARY KEY  (clovek_ID, rocnik),
   KEY clovek_ID (clovek_ID),
   constraint fk_rozhodci_clovek foreign key (clovek_ID) references clovek (clovek_ID) on delete cascade
 ) TYPE=InnoDB CHARSET=utf8 COLLATE=utf8_czech_ci;
@@ -280,10 +271,9 @@ CREATE TABLE teze (
 --
 
 CREATE TABLE soutez_teze (
-  st_ID int(10) unsigned NOT NULL auto_increment,
   soutez_ID int(10) unsigned NOT NULL default '0',
   teze_ID int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (st_ID),
+  PRIMARY KEY  (soutez_ID, teze_ID),
   KEY soutez_ID (soutez_ID),
   KEY teze_ID (teze_ID),
   constraint fk_st_soutez foreign key (soutez_ID) references soutez (soutez_ID) on delete cascade,
